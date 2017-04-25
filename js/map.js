9b674a0c5b2c56ce4c4c621c809c264e22db679b
nanter1986 <nanter1986@gmail.com>
2017-04-25 20:54:14 +0300

    images same row

--- js/map.js
+++ js/map.js
@@ -2,25 +2,33 @@
  * Created by user on 25/4/2017.
  */
 var w = window.innerWidth;
-var tileW=w/6;
+var tileW=w/4.1;
 console.log(tileW);
 
 
-function makeTiles(imageName,col) {
+function makeTiles(imageName,col,where) {
     var tile=document.createElement("img");
     tile.setAttribute("src","../img/"+imageName+".jpg");
     tile.setAttribute("height",tileW);
     tile.setAttribute("width",tileW);
     tile.setAttribute("left",col*tileW);
-    document.getElementById("div1").appendChild(tile);
+    document.getElementById(where).appendChild(tile);
 
 }
 
 function addImages() {
-    makeTiles("agora",0);
-    makeTiles("crete8",1);
-    makeTiles("splantzia",2);
-    makeTiles("xeri",3);
+    makeTiles("agora",0,"div1");
+    makeTiles("crete8",1,"div1");
+    makeTiles("splantzia",2,"div1");
+    makeTiles("xeri",3,"div1");
+    makeTiles("agora",0,"div2");
+    makeTiles("crete8",1,"div2");
+    makeTiles("splantzia",2,"div2");
+    makeTiles("xeri",3,"div2");
+    makeTiles("agora",0,"div3");
+    makeTiles("crete8",1,"div3");
+    makeTiles("splantzia",2,"div3");
+    makeTiles("xeri",3,"div3");
 }
 
 
