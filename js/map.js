14121ec796f925a06df1c4d2ea5af0de16597bbe
nanter1986 <nanter1986@gmail.com>
2017-04-25 20:45:32 +0300

    images same row

--- js/map.js
+++ js/map.js
@@ -2,9 +2,26 @@
  * Created by user on 25/4/2017.
  */
 var w = window.innerWidth;
+var tileW=w/6;
+console.log(tileW);
 
-function makeTiles() {
-    var tiles=document.getElementsByClassName("roomsentrance");
-    tiles.style.width=w/6;
-    tiles.style.height=w/6;
+
+function makeTiles(imageName,col) {
+    var tile=document.createElement("img");
+    tile.setAttribute("src","../img/"+imageName+".jpg");
+    tile.setAttribute("height",tileW);
+    tile.setAttribute("width",tileW);
+    tile.setAttribute("left",col*tileW);
+    document.getElementById("div1").appendChild(tile);
+
 }
+
+function addImages() {
+    makeTiles("agora",0);
+    makeTiles("crete8",1);
+    makeTiles("splantzia",2);
+    makeTiles("xeri",3);
+}
+
+
+
