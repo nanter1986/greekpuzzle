f1cc242354dfafeb8ae9d73e73560667fadd310b
nanter1986 <nanter1986@gmail.com>
2017-04-27 19:31:51 +0300

    abstraction of links

--- js/map.js
+++ js/map.js
@@ -6,29 +6,33 @@ var tileW=w/4.1;
 console.log(tileW);
 
 
-function makeTiles(imageName,col,where) {
+function makeTiles(imageName,col,where,theLink) {
     var tile=document.createElement("img");
     tile.setAttribute("src","../img/"+imageName+".jpg");
     tile.setAttribute("height",tileW);
     tile.setAttribute("width",tileW);
     tile.setAttribute("left",col*tileW);
+    tile.setAttribute("style","border-radius:20px");
+    tile.setAttribute("onclick",theLink);
     document.getElementById(where).appendChild(tile);
 
 }
 
+function goToLink(name){
+    if(name==="faros"){
+        var link="../index.html";
+        window.open(link,"_self");
+    }else{
+        var link="../html/"+name+".html";
+        window.open(link,"_self");
+    }
+}
+
 function addImages() {
-    makeTiles("agora",0,"div1");
-    makeTiles("crete8",1,"div1");
-    makeTiles("splantzia",2,"div1");
-    makeTiles("xeri",3,"div1");
-    makeTiles("agora",0,"div2");
-    makeTiles("crete8",1,"div2");
-    makeTiles("splantzia",2,"div2");
-    makeTiles("xeri",3,"div2");
-    makeTiles("agora",0,"div3");
-    makeTiles("crete8",1,"div3");
-    makeTiles("splantzia",2,"div3");
-    makeTiles("xeri",3,"div3");
+    makeTiles("agora",0,"div1","goToLink('agora')");
+    makeTiles("faros",1,"div1","goToLink('faros')");
+    makeTiles("splantzia",2,"div1","goToLink('splantzia')");
+    makeTiles("xeri",3,"div1","goToLink('xeri')");
 }
 
 
